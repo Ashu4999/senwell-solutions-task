6ab83f14be2109501fc868486025cdee8e96c6fd
Ashutosh Gorke <ashutosh@technative.in>
2023-07-11 22:06:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [nameCountData, setNameCountData] = useState(null);
+
+  async function getData() {
+    try {
+      let response = await axios.get(
+        "https://api.slingacademy.com/v1/sample-data/users?&limit=50"
+      );
+      setData(response.data.users);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function makeDublicateData() {
+    let limit = 150;
+    let dublicateData = data;
+    for (let i = 0; i < limit; i++) {
+      let randomIndex = Math.floor(Math.random() * 20);
+      dublicateData.push({...data[randomIndex], id: dublicateData.length + 1});
+    }
+
+    let nameCountObj = {};
+    dublicateData.forEach(obj => {
+      let name = obj.first_name;
+      nameCountObj[name] = nameCountObj[name] ? nameCountObj[name] + 1 : 1;
+    })
+    setNameCountData(nameCountObj)
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    data.length > 0 && makeDublicateData();
+  }, [data]);
+
+  const getRowBackground = count => {
+    if (count > 0 && count < 3) {
+      return "red";
+    } else if (count >= 3 && count < 10) {
+      return "yellow";
+    } else if (count >= 10) {
+      return "green";
+    }
+    return "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Task</p>
+      {
+        nameCountData && 
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Count</th>
+              </tr>
+              {
+                Object.keys(nameCountData).map((name, index) => {
+                  return(
+                    <tr key={index} style={{ backgroundColor: getRowBackground(nameCountData[name]) }}>
+                      <td>{name}</td>
+                      <td>{nameCountData[name]}</td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+      }
     </div>
   );
 }
